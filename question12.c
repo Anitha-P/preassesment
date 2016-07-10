8a7d396a99f47253a21653752e5b3ead385e311f
Anitha-P <anitha95anitha@gmail.com>
2016-07-10 12:47:41 +0530

    Create question12.c

---
+++ question12.c
@@ -0,0 +1,32 @@
+# recursive
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name={"TREE"};
+int thickness;
+int branches;
+char color[];
+int angle;
+printf("enter the thickness of the tree");
+scanf("%d,&thickness of the tree");
+printf("enter the branches of the tree");
+scanf("%d,branches of the tree");
+printf("enter the color of the tree");
+scanf("%c,&color of the tree");
+printf("enter the angle of the tree");
+scanf("%d,&angle of the tree");
+if(thickness>23)
+{
+if (branches>20)
+{
+if(angle<=45)
+{
+printf("the angle of the tree is:");
+}
+printf("the branches of the tree is:");
+}
+printf(" the thickness of the tree is:");
+}
+getch()
+}
