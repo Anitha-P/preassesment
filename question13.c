9fc304b969e28303ed981813db8ef622bf19034c
Anitha-P <anitha95anitha@gmail.com>
2016-07-10 11:11:15 +0530

    Create question13.c

---
+++ question13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+
+void towerofhanoi(int num,char from,char to,char aux)
+{
+    if(num==1)
+    {
+        printf("\nMoving disc 1 from peg %c to peg %c",from,to);
+        return;
+    }
+    towerofhanoi(num-1,frompeg,auxpeg,topeg);
+    printf("\nMoving disc %d from peg %c to peg %c",num,from,to);
+    towerofhanoi(num-1,to,from,aux);
+}
+
+int main(){
+    int num;
+    scanf("%d",&num);
+    towerofhanoi(num,'A','C','B');
+    return 0;
+}
